560f2f0c5cb37857678398798620f17afdac79ed
RamilBinnatov <ramilrb@code.edu.az>
2022-10-31 00:36:33 +0400

    CRUD

---
+++ Fiorello-backend/EntityFrameworkProject/Migrations/20221028131949_CreateBlogHeaderTable.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameworkProject.Migrations
+{
+    public partial class CreateBlogHeaderTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BlogHeaders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    Header = table.Column<string>(nullable: true),
+                    HeaderDesc = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogHeaders", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2022, 10, 28, 17, 19, 49, 597, DateTimeKind.Local).AddTicks(8745));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2022, 10, 28, 17, 19, 49, 599, DateTimeKind.Local).AddTicks(1252));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2022, 10, 28, 17, 19, 49, 599, DateTimeKind.Local).AddTicks(1293));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlogHeaders");
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2022, 10, 24, 19, 6, 36, 982, DateTimeKind.Local).AddTicks(4858));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2022, 10, 24, 19, 6, 36, 983, DateTimeKind.Local).AddTicks(3089));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2022, 10, 24, 19, 6, 36, 983, DateTimeKind.Local).AddTicks(3112));
+        }
+    }
+}
